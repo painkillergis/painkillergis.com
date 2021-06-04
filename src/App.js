6f82a8f7d9a0cd7c2d89f0fa83702e1bc739b15e
tyler <tyler@cruftbusters.com>
2021-06-04 13:55:51 -0500

    dissolve brand into app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Brand from './components/Brand'
+import brand from './components/brand.svg'
 import Guttered from './components/Guttered'
 import Splash from './components/Splash'
 import { Paper } from './components/Paper'
@@ -9,7 +9,24 @@ function App() {
       <Splash />
       <Guttered>
         <Paper>
-          <Brand />
+          <img
+            style={{
+              float: 'left',
+              width: '75px',
+              filter: 'drop-shadow(2px 2px 1px #555)',
+            }}
+            src={brand}
+            alt="brand"
+          />
+          <span
+            style={{
+              fontSize: '36px',
+              margin: '25px',
+              marginLeft: '12.5px',
+            }}
+          >
+            tinfoil{' '}
+          </span>
           <div>
             <p>Learn topography with fun 3D puzzles</p>
           </div>
